ffcd4d7540d939aa68b3c7f86a89567a1f2e95b7
Gabriel Boalento <gabrielboalento3004@gmail.com>
2026-08-24 10:29:38 -0300

    feat: load ingame overlay styles

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import './styles/overlay.css'
 import './styles/client-fit.css'
 import './styles/client-motion.css'
 import './styles/transitions.css'
+import './styles/ingame.css'
 import './debug'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
