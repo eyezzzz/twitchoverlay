a6d112aa10a7a4b176aed83fd940c34fd94b45da
Gabriel Boalento <gabrielboalento3004@gmail.com>
2026-08-24 10:27:43 -0300

    feat: simplify ingame overlay to cam and chat

--- src/scenes/InGameScene.tsx
+++ src/scenes/InGameScene.tsx
@@ -1,16 +1,11 @@
 import { ChatPanel } from '../components/ChatPanel'
 import { WebcamFrame } from '../components/WebcamFrame'
-import { Brand } from '../components/Brand'
 
 export function InGameScene() {
   return (
     <main className="overlay overlay--ingame">
       <WebcamFrame mode="ingame" />
       <ChatPanel compact />
-      <div className="ingame-signature" aria-hidden="true">
-        <Brand compact />
-        <span className="vision-mark" />
-      </div>
     </main>
   )
 }
