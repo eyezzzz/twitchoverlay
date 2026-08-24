2fcd2be4e8aea3a5e5fe2abe604c3ce49f333d12
Gabriel Boalento <gabrielboalento3004@gmail.com>
2026-08-24 08:00:36 -0300

    feat: add client neon orbit and animated socials

--- src/components/ClientChrome.tsx
+++ src/components/ClientChrome.tsx
@@ -22,12 +22,49 @@ export function ClientChrome() {
         <span className="edge edge--tr" />
         <span className="edge edge--br" />
         <span className="edge edge--bl" />
+
+        <svg
+          className="client-comet"
+          viewBox="0 0 1000 1000"
+          preserveAspectRatio="none"
+          focusable="false"
+          aria-hidden="true"
+        >
+          <defs>
+            <filter id="client-comet-glow" x="-30%" y="-30%" width="160%" height="160%">
+              <feGaussianBlur stdDeviation="5" result="blur" />
+              <feMerge>
+                <feMergeNode in="blur" />
+                <feMergeNode in="SourceGraphic" />
+              </feMerge>
+            </filter>
+          </defs>
+          <rect className="client-comet__trail" x="4" y="4" width="992" height="992" pathLength="1000" />
+          <rect className="client-comet__core" x="4" y="4" width="992" height="992" pathLength="1000" />
+          <rect className="client-comet__spark" x="4" y="4" width="992" height="992" pathLength="1000" />
+        </svg>
+      </div>
+
+      <div className="livepix-dock" aria-hidden="true">
+        <span className="livepix-dock__corner livepix-dock__corner--tl" />
+        <span className="livepix-dock__corner livepix-dock__corner--br" />
+        <span className="livepix-dock__label">LIVEPIX</span>
       </div>
 
       <div className="client-footer" aria-hidden="true">
-        <span className="client-footer__role">FIETZLOL / SUPPORT</span>
-        <div className="client-footer__line" />
-        <span className="client-footer__signature">VISION • MAP • TEAM</span>
+        <div className="social-rail">
+          <div className="social-item social-item--x">
+            <span className="social-item__platform">X</span>
+            <span className="social-item__handle">@fietzlol</span>
+          </div>
+          <span className="social-rail__divider" />
+          <div className="social-item social-item--instagram">
+            <span className="social-item__platform">IG</span>
+            <span className="social-item__handle">@fietzlol</span>
+          </div>
+        </div>
+        <div className="client-footer__line"><span /></div>
+        <span className="client-footer__signature">SUPPORT • VISION • TEAM</span>
       </div>
     </>
   )
