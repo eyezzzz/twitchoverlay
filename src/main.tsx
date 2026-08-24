39e5e718742b0389f5ca33ce2951dc8a7ad0b535
Gabriel Boalento <gabrielboalento3004@gmail.com>
2026-08-24 07:13:56 -0300

    fix: load exact client fit override

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App'
 import './styles/tokens.css'
 import './styles/global.css'
 import './styles/overlay.css'
+import './styles/client-fit.css'
 import './debug'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
