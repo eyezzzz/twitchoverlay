0c4f6abba8c6046f17acc05cc29b4b8054ac655f
Gabriel Boalento <gabrielboalento3004@gmail.com>
2026-08-24 09:19:38 -0300

    feat: route OBS transition scenes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { ClientScene } from './scenes/ClientScene'
 import { InGameScene } from './scenes/InGameScene'
+import { TransitionScene } from './scenes/TransitionScene'
 
 function normalizePath(pathname: string) {
   return pathname.replace(/\/+$/, '') || '/'
@@ -9,5 +10,9 @@ export default function App() {
   const path = normalizePath(window.location.pathname)
 
   if (path === '/ingame') return <InGameScene />
+  if (path === '/starting') return <TransitionScene mode="starting" />
+  if (path === '/brb') return <TransitionScene mode="brb" />
+  if (path === '/ending') return <TransitionScene mode="ending" />
+  if (path === '/stinger') return <TransitionScene mode="stinger" />
   return <ClientScene />
 }
